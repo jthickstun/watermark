5545ab19cfedd4fbad807956c0e1abf5653b7880
Rohith Kuditipudi <rohith.kuditipudi@gmail.com>
2023-08-15 16:00:47 -0700

    cleanup

--- detect.py
+++ detect.py
@@ -9,36 +9,32 @@ pyximport.install(reload_support=True, language_level=sys.version_info[0],
                   setup_args={'include_dirs':np.get_include()})
 from levenshtein import levenshtein
 
-def permutation_test(tokens,vocab_size,key,n,k,n_runs=100,max_seed=100000):
+def permutation_test(tokens,key,n,k,vocab_size,n_runs=100):
     rng = mersenne_rng(key)
     xi = np.array([rng.rand() for _ in range(n*vocab_size)], dtype=np.float32).reshape(n,vocab_size)
-    test_result = detect(tokens,n,k,xi,vocab_size)
-
-    unique_tokens, _ = np.unique(tokens, return_inverse=True)
-    eff_vocab_size = np.max(unique_tokens) + 1
+    test_result = detect(tokens,n,k,xi)
 
     p_val = 0
     for run in range(n_runs):
         xi_alternative = np.random.rand(n, vocab_size).astype(np.float32)
-        null_result = detect(tokens,n,k,xi_alternative,vocab_size)
+        null_result = detect(tokens,n,k,xi_alternative)
 
         # assuming lower test values indicate presence of watermark
-        p_val += null_result < test_result
+        p_val += null_result <= test_result
 
     return (p_val+1.0)/(n_runs+1.0)
 
 
-def detect(tokens,n,k,xi,vocab_size,gamma=0.0,null=False):
+def detect(tokens,n,k,xi,gamma=0.0):
     m = len(tokens)
     n = len(xi)
 
     A = np.empty((m-(k-1),n))
     for i in range(m-(k-1)):
         for j in range(n):
-            A[i][j] = levenshtein(tokens[i:i+k],xi[(j+np.arange(k))%n])
+            A[i][j] = levenshtein(tokens[i:i+k],xi[(j+np.arange(k))%n],gamma)
 
-    closest = np.min(A[:,:n],axis=1)[0]
-    return np.median(closest)
+    return np.min(A)
 
 
 def main(args):
@@ -49,7 +45,7 @@ def main(args):
     tokens = tokenizer.encode(text, return_tensors='pt', truncation=True, max_length=2048).numpy()[0]
     
     t0 = time.time()
-    pval = permutation_test(tokens,len(tokenizer),args.key,args.n,len(tokens))
+    pval = permutation_test(tokens,args.key,args.n,len(tokens),len(tokenizer))
     print('p-value: ', pval)
     print(f'(elapsed time: {time.time()-t0}s)')
 
